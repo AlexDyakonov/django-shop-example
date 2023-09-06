74d91fc27b2225c252b70e1703928913e64da8ae
Alex_D <shram.monolit@mail.ru>
2023-09-06 21:48:32 +0300

    Made soft appearing of top menu

--- shop/static/assets/js/main.js
+++ shop/static/assets/js/main.js
@@ -1,14 +1,16 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
     const headerHeight = header.offsetHeight;
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollTop = document.documentElement.scrollTop;
   
     if (scrollTop < headerHeight * 2){
       header.classList.remove('header-hidden')
       header.classList.remove('header-sticky')
-    } else if (scrollTop > headerHeight * 2 && scrollTop < headerHeight * 3){
+    
+    } else if (scrollTop > headerHeight * 2 && scrollTop < headerHeight * 7){
       header.classList.add('header-hidden');
-    } if (scrollTop > headerHeight * 3){
+
+    } if (scrollTop > headerHeight * 7){
       header.classList.remove('header-hidden');
       header.classList.add('header-sticky')
     }
