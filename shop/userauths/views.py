fca00499c3f384ab0960da28c17aed707f587f13
Alex_D <shram.monolit@mail.ru>
2023-09-08 01:54:03 +0300

    Added registration page

--- shop/userauths/views.py
+++ shop/userauths/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+
 
 def register_view(request):
-    return render(request, "userauths/sign-up.html")
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST or None)
+
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f"Hey {username}, your account was created successfully.")
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1']
+                                    )
+            login(request, new_user)
+            return redirect("core:home")
+        
+    else:
+        form = UserRegisterForm()    
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, "userauths/sign-up.html", context)
