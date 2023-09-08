984eb2020e7e5dac4449811b53ac9b2d55a70f8d
Alex_D <shram.monolit@mail.ru>
2023-09-08 11:19:05 +0300

    Realised log in feature

--- shop/userauths/views.py
+++ shop/userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 def register_view(request):
@@ -27,3 +30,32 @@ def register_view(request):
     }
 
     return render(request, "userauths/sign-up.html", context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "Already logged in.")
+        return redirect("core:home")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exist.")
+
+        user = authenticate(request, email = email, password = password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in!")
+            return redirect("core:home")
+        else:
+            messages.warning(request, "User does not exist, create an account.")
+
+    context = {
+
+    }
+
+    return render(request, "userauths/sign-in.html", context)
