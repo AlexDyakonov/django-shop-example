5bb1f40fb575f1530dd213e13488f140761593e0
AlexDyakonov <shram.monolit@mail.ru>
2023-09-08 16:36:52 +0300

    Translated everything to russian. Added alerts. Small fixes of html

--- shop/userauths/forms.py
+++ shop/userauths/forms.py
@@ -3,10 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Username"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Никнейм"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder" : "Email"}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm password"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Пароль"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Подтвердите пароль"}))
 
     class Meta:
         model = User
--- shop/userauths/views.py
+++ shop/userauths/views.py
@@ -3,8 +3,9 @@ from userauths.forms import UserRegisterForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.conf import settings
+from userauths.models import User
 
-User = settings.AUTH_USER_MODEL
+# User = settings.AUTH_USER_MODEL
 
 
 def register_view(request):
@@ -15,7 +16,7 @@ def register_view(request):
         if form.is_valid():
             new_user = form.save()
             username = form.cleaned_data['username']
-            messages.success(request, f"Hey {username}, your account was created successfully.")
+            messages.success(request, f"{username}, ваш аккаунт успешно создан.")
             new_user = authenticate(username=form.cleaned_data['email'],
                                     password=form.cleaned_data['password1']
                                     )
@@ -33,7 +34,7 @@ def register_view(request):
 
 def login_view(request):
     if request.user.is_authenticated:
-        messages.warning(request, "Already logged in.")
+        messages.warning(request, "Вы уже вошли в аккаунт.")
         return redirect("core:home")
     
     if request.method == "POST":
@@ -42,17 +43,16 @@ def login_view(request):
 
         try:
             user = User.objects.get(email=email)
+            user = authenticate(request, email = email, password = password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Вы уже вошли в аккаунт!")
+                return redirect("core:home")
+            else:
+                messages.warning(request, "Данный пользователь не существует, зарегестрируйтесь.")
         except:
-            messages.warning(request, f"User with {email} does not exist.")
-
-        user = authenticate(request, email = email, password = password)
-
-        if user is not None:
-            login(request, user)
-            messages.success(request, "You are logged in!")
-            return redirect("core:home")
-        else:
-            messages.warning(request, "User does not exist, create an account.")
+            messages.warning(request, f"Пользователя с почтой {email} не существует.")
 
     context = {
 
@@ -62,7 +62,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    messages.success(request, "Logged out.")
+    messages.success(request, "Вы успешно вышли из аккаунта.")
 
     return redirect("userauths:sign-in")
 
